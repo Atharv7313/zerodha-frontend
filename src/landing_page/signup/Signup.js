7f6f62eaa6c67f42c11cc8cd9ba09bec14c69bb2
Atharv7313 <137501939+Atharv7313@users.noreply.github.com>
2025-07-29 23:44:56 +0530

    Update Signup.js

--- src/landing_page/signup/Signup.js
+++ src/landing_page/signup/Signup.js
@@ -41,7 +41,7 @@ function SignUp() {
             if(success){
                 handleSuccess(message);
                 setTimeout(() => {
-                    window.location.href = "http://localhost:3000";
+                    window.location.href = "https://zerodha-dashboard-liart.vercel.app";
                 }, 1000);
             }else{
                 handleError(message)
